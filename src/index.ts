73e062affab322d0b41231dd6cc4f21155382c9f
Alex Kalinins <jmal2424@gmail.com>
2021-12-30 11:35:03 -0500

    Add rate limiter

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,37 @@ import serverless from "serverless-http";
 import ClusterRoute from './routes/cluster_radio';
 import SpotifyRoute from './routes/spotify';
 import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit'
+
 
 dotenv.config();
 
 const app = express();
 const PORT = 3001;
+
+const nebulaLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // tell client about rate limit
+  legacyHeaders: false
+});
+
+const radioLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  standardHeaders: true, // tell client about rate limit
+  legacyHeaders: false
+});
+
  
 app.get('/', async function (req, res) {
     res.send('Hello World!');
 })
 
+app.use('/spotify', nebulaLimiter);
 app.use('/spotify', SpotifyRoute);
+
+app.use('/cluster_radio', radioLimiter);
 app.use('/cluster_radio', ClusterRoute);
 
 app.listen( PORT, () => {
